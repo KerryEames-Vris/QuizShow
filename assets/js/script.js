16b332835d1a58f553fd32218bd16546b538259d
Kerry Eames <Kerryeames@live.com>
2021-11-11 21:11:30 -0700

    made several variables

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var startBtn = $("#startBtn");
+var clickBtn1 = $("#examBtn1");
+var clickBtn2 = $("#examBtn2");
+var clickBtn3 = $("#examBtn3");
+var clickBtn4 = $("#examBtn4");
+
+var examQuestion = $("#examQuestion");
