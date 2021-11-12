000d2db58b3567801d867c4108aa5ee94bcf3737
Kerry Eames <Kerryeames@live.com>
2021-11-11 21:42:11 -0700

    did some psuedo code, set up the basis of functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,24 @@ var clickBtn3 = $("#examBtn3");
 var clickBtn4 = $("#examBtn4");
 
 var examQuestion = $("#examQuestion");
+
+//need an event to start the quiz
+
+startBtn.eventlistener("click", startQuiz());
+//need to hide start button after clicking it
+
+function startBtnHide () {
+    startBtn.style.visibility = "hidden";
+}
+//need to populate question and answers
+
+function startQuiz() {
+    startBtnHide();
+
+}
+
+//need an event to select answers
+
+if 
+
+//need an event move to next question
